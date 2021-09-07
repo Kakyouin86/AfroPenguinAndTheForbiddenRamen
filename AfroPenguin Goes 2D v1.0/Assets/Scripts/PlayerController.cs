5f7f3813c3150667437f229d2d34786f90810d6f
Leonardo Jorge Monge <leo.monge1986@gmail.com>
2021-09-06 22:29:37 -0300

    1

--- AfroPenguin Goes 2D v1.0/Assets/Scripts/PlayerController.cs
+++ AfroPenguin Goes 2D v1.0/Assets/Scripts/PlayerController.cs
@@ -5,21 +5,51 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
+    [Header("Movement")]
     public float moveSpeed = 8.0f;
     //public Rigidbody2D theRB;
     public float jumpForce = 15.0f;
-    public bool isGrounded;
-    public bool canDoubleJump;
-    public Transform groundCheckPoint;
+    public float doubleJumpForce = 15.0f;
     public LayerMask whatIsGround;
+    public bool isGrounded;
+    public bool wasDoubleJump;
+    public bool canDoubleJump;
+    public bool stopInput;
+    public bool facingRight;
+    public Transform groundCheckPoint;
     //public Vector2 jumpHeight;
+
+    [Header("Animations & Sprite")]
     private Animator anim;
     public SpriteRenderer sr;
+
+    [Header("Knockback")]
     public float knockbackLenght = 0.25f;
     public float knockbackForce = 5.0f;
     public float knockbackCounter;
+
+    [Header("Bounce After Jump")]
     public float bounceForce = 15.0f;
-    public bool stopInput;
+
+
+    //NEW tricks:
+    //public float hangTime = 0.2f;
+    //public float hangCounter;
+
+    //public float jumpBufferLength = 0.5f; //from where we are pressing the Jump button
+    //public float jumpBufferCounter;
+
+    //Little trick on seeing ahead:
+
+    [Header("Mario Style Camera")]
+    public Transform camTarget;
+    public float aheadAmount = 1.5f;
+    public float aheadSpeed = 1f;
+
+    //Dust effect
+
+    [Header("Particle System (Dust)")]
+    public ParticleSystem dust;
 
     private void Awake()
     {
@@ -34,71 +64,75 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (!PauseMenu.instance.isPaused && !stopInput)
-            //if the pause is false (we are playing) AND, we haven't stopped our input, then... move the player. If one of them true, we can't do ANY of this stuff. If both are true, then it won't move.
+        //if the pause is false (we are playing) AND, we haven't stopped our input, then... move the player. If one of them true, we can't do ANY of this stuff. If both are true, then it won't move.
         {
             MovePlayer();
             //Debug.Log(transform.position);
         }
-
-
     }
     void MovePlayer()
     {
         if (knockbackCounter <= 0)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal"), GetComponent<Rigidbody2D>().velocity.y);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
             isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, 0.2f, whatIsGround);
+
             if (isGrounded)
             {
                 canDoubleJump = true;
+                wasDoubleJump = false;
+                //NEW
+                //hangCounter = hangTime;
+                //Stop NEW
             }
-            //rb.velocity = new Vector2(moveSpeed * Input.GetAxisRaw("Horizontal") , rb.velocity.y);
-            //Esto es si quisiera que el movimiento del personaje sea menos "suave".
-            //GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed * Input.GetAxis("Horizontal") , GetComponent<Rigidbody2D>().velocity.y);
 
-            //float verticalMovement = Input.GetAxis("Vertical" + playerIndex);
-            //GetComponent<Rigidbody2D>().velocity = new Vector2(0, verticalMovement * speed);
-
-            //float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
-            //float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
-            //Debug.Log(xValue);
-            //Debug.Log(zValue);
-            //transform.Translate(xValue, yValue, zValue);
-
-            //if (Input.GetKey(KeyCode.Q))
+            //NEW
+            //else
             //{
-            //    GetComponent<Rigidbody2D>().AddForce(jumpHeight, ForceMode2D.Impulse);
+            //hangCounter -= Time.deltaTime;
             //}
+            //Stop NEW
 
-            if (Input.GetButtonDown("Jump"))
+            if (Input.GetButtonDown("Jump")) //&& hangCounter > 0f)  //Second part is NEW and it's for the hangCounter in case double jump is not an option.
             {
                 if (isGrounded)
                 {
                     GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpForce);
                     AudioManager.instance.PlaySFX(10);
+                    isGrounded = false;
+                    wasDoubleJump = false;
+                    CreateDust();
                 }
 
-                else
-                    if (canDoubleJump)
+                else if (canDoubleJump)
                 {
-                    GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpForce);
+                    wasDoubleJump = true;
+                    GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, doubleJumpForce);
                     canDoubleJump = false;
                     AudioManager.instance.PlaySFX(10);
                 }
             }
 
+            //NEW:
+            if (Input.GetButtonUp("Jump") && GetComponent<Rigidbody2D>().velocity.y > 0)
+            {
+                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y * 0.5f);
+            }
+            //Stop NEW
+
             if (GetComponent<Rigidbody2D>().velocity.x < 0)
             {
                 sr.flipX = true;
+                facingRight = false;
             }
             else if (GetComponent<Rigidbody2D>().velocity.x > 0)
             {
                 sr.flipX = false;
+                facingRight = true;
             }
         }
 
         else
-
         {
             knockbackCounter -= Time.deltaTime;
             //this counts down my time.
@@ -112,6 +146,12 @@ public class PlayerController : MonoBehaviour
             {
                 GetComponent<Rigidbody2D>().velocity = new Vector2(knockbackForce, GetComponent<Rigidbody2D>().velocity.y);
             }
+        }
+
+        //Camera Trick: Set a target as a child of the player so it moves either left or right when switching positions
+        if (Input.GetAxisRaw("Horizontal") != 0)
+        {
+            camTarget.localPosition = new Vector3(Mathf.Lerp(camTarget.localPosition.x, aheadAmount * Input.GetAxisRaw("Horizontal"), aheadSpeed * Time.deltaTime), camTarget.localPosition.y, camTarget.localPosition.z);
         }
 
         anim.SetFloat("moveSpeed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
@@ -129,5 +169,10 @@ public class PlayerController : MonoBehaviour
     {
         GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, bounceForce);
         AudioManager.instance.PlaySFX(10);
+    }
+
+    public void CreateDust()
+    {
+        dust.Play();
     }
 }
---
+++ AfroPenguin Goes 2D v1.0/Assets/_Scripts/PlayerCon2troller.cs
@@ -0,0 +1,209 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.Tilemaps;
+
+public class PlayerCon2troller : MonoBehaviour{
+    public ParticleSystem dust;
+
+    [Header("Movement")]
+    public float horizontalVelocity = 10f;
+    public float maxWalk = 3f;
+    public float maxSpeed = 7f;
+    public float linearDrag = 7f;
+    private Vector2 direction;
+
+    [Header("Jumping")]
+    public float jumpVelocity = 8f;
+    public float fallMultiplier = 5f;
+    public float gravity = 1f;
+    public float maxGravity = 10f;
+    public float wallJumpLerp = 10f;
+    private float jumpPressedDown = -1f;
+    private bool wallJumped = false;
+
+    [Header("UI")]
+    public TextMeshProUGUI controllerText;
+
+    [Header("Layers")]
+    public LayerMask groundLayer;
+
+    public bool onGround;
+    public bool canJump;
+    public bool onWall;
+    public bool onRightWall;
+    public bool onLeftWall;
+    public int wallSide;
+    private float runTrigger;
+
+    [Space]
+
+    [Header("Collision")]
+
+    public float collisionRadius = 0.25f;
+    public Vector2 groundOffset, rightOffset, leftOffset;
+    private Color debugCollisionColor = Color.red;
+
+    [Space]
+
+    [Header("Animation")]
+    public GameObject characterSprite;
+    public GameObject trail;
+
+    //PLAYER COMPONENTS
+    private Rigidbody2D rb;
+    private Animator animator;
+    private bool facingRight = true;
+
+    // Start is called before the first frame update
+    void Awake(){
+        rb = GetComponent<Rigidbody2D>();
+        animator = characterSprite.GetComponentInChildren<Animator>();
+    }
+
+    void Update(){
+        bool wasOnGround = onGround;
+        //collison detection
+        onGround = Physics2D.Raycast(new Vector2(transform.position.x + groundOffset.x, transform.position.y - groundOffset.y + 0.1f), Vector2.down, 0.1f, groundLayer) || Physics2D.Raycast(new Vector2(transform.position.x - groundOffset.x, transform.position.y - groundOffset.y + 0.1f), Vector2.down, 0.1f, groundLayer);
+        onRightWall = Physics2D.OverlapCircle((Vector2)transform.position + rightOffset, collisionRadius, groundLayer);
+        onLeftWall = Physics2D.OverlapCircle((Vector2)transform.position + leftOffset, collisionRadius, groundLayer);
+        onWall = onRightWall || onLeftWall;
+        wallSide = onRightWall ? -1 : 1;
+
+        //landed
+        if(!wasOnGround && onGround){
+            StartCoroutine(JumpSqueeze(1.25f, 0.8f, 0.05f));
+        }
+        if (onGround || !onWall){
+            wallJumped = false;
+        }
+
+        if(Input.GetButtonDown("Jump")){
+            jumpPressedDown = Time.time + 0.25f;
+        }
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        runTrigger = Input.GetAxisRaw("WallHold");
+
+        animator.SetBool("onGround", onGround);
+    }
+    void FixedUpdate(){
+        //MOVEMENT
+        moveCharacter(direction.x);
+        bool jumpTrigger = Time.time < jumpPressedDown;
+
+        //JUMPING
+        if (jumpTrigger && onGround) {
+            Jump(Vector2.up * jumpVelocity);
+        }else if (jumpTrigger && !wallJumped && ((direction.x > 0.5f && onLeftWall) || (direction.x < -0.5f && onRightWall))) { //wallJump
+            Jump(new Vector2(maxSpeed * 0.75f * wallSide, jumpVelocity * 0.75f));
+            wallJumped = true;
+        }
+        //GRAVITY & WALL GRABS
+        if (onWall){
+            rb.gravityScale = gravity * 0.5f;
+        } else if (onGround) {
+            rb.gravityScale = 0f;
+        } else {
+            rb.gravityScale = gravity;
+        }
+        //CONTROLLING VERTICAL VELOCITY
+        if (rb.velocity.y < 0){
+            rb.gravityScale = gravity * fallMultiplier;
+        }else if(rb.velocity.y > 0 && !Input.GetButton("Jump")){
+            rb.gravityScale = gravity * fallMultiplier * 0.5f;
+        }
+        //prevent falling too fast
+        if (rb.velocity.y < -maxGravity){
+            rb.velocity = new Vector2(rb.velocity.x, Mathf.Sign(rb.velocity.y) * maxGravity);
+        }
+
+        animator.SetFloat("horizontal", Mathf.Abs(rb.velocity.x));
+        animator.SetFloat("vertical", rb.velocity.y);
+        animator.SetBool("moving", Mathf.Abs(direction.x) > 0.5f);
+        controllerText.SetText("X:" + rb.velocity.x + "\nY: " + rb.velocity.y);
+    }
+    void moveCharacter(float horizontal){
+        bool runButton = runTrigger > 0;
+        //control speed
+        if ((runButton && horizontal * rb.velocity.x < maxSpeed) || (!runButton && horizontal * rb.velocity.x < maxWalk)){
+            rb.AddForce(Vector2.right * horizontal * horizontalVelocity);
+        }
+        if (runButton && Mathf.Abs(rb.velocity.x) > maxSpeed){
+            rb.velocity = new Vector2(Mathf.Sign(rb.velocity.x) * maxSpeed, rb.velocity.y);
+        }
+        if(Mathf.Abs(rb.velocity.x) > maxWalk){
+            trail.SetActive(true);
+        }else{
+            trail.SetActive(false);
+        }
+        bool changingDirections = (horizontal > 0 && rb.velocity.x < 0) || (horizontal < 0 && rb.velocity.x > 0);
+
+        //calculate drag
+        if (!onGround){
+            if (onWall && Mathf.RoundToInt(horizontal) == wallSide * -1) {
+                rb.drag = linearDrag;
+            }else{
+                rb.drag = linearDrag * 0.15f;
+            }
+        } else{
+
+            if(Mathf.Abs(horizontal) < 0.4f && !changingDirections){
+                rb.drag = linearDrag;
+            }else{
+                rb.drag = linearDrag * 0.01f;
+            }
+            if (changingDirections){
+                rb.AddForce(Vector2.right * horizontal * (horizontalVelocity * 0.75f));
+            }
+        }
+        if(onWall){
+            if((onRightWall && facingRight) || (onLeftWall && !facingRight)){
+                Flip(!facingRight);
+            }
+        }else if ((horizontal > 0 && !facingRight) || (horizontal < 0 && facingRight)){
+            Flip(!facingRight);
+        }
+    }
+    void Jump(Vector2 force){
+        CreateDust();
+        rb.velocity = new Vector2(rb.velocity.x, 0);
+        rb.AddForce(force, ForceMode2D.Impulse);
+        jumpPressedDown = -1f;
+        StartCoroutine(JumpSqueeze(0.5f, 1.2f, 0.1f));
+    }
+    IEnumerator JumpSqueeze(float xSqueeze, float ySqueeze, float seconds){
+        Vector3 originalSize = Vector3.one;
+        Vector3 newSize = new Vector3(xSqueeze, ySqueeze, originalSize.z);
+        float t = 0f;
+        while (t <= 1.0) {
+            t += Time.deltaTime / seconds;
+            characterSprite.transform.localScale = Vector3.Lerp(originalSize, newSize, t);
+            yield return null;
+        }
+        t = 0f;
+        while (t <= 1.0) {
+            t += Time.deltaTime / seconds;
+            characterSprite.transform.localScale = Vector3.Lerp(newSize, originalSize, t);
+            yield return null;
+        }
+
+    }
+    void Flip(bool right){
+        CreateDust();
+        facingRight = right;
+        transform.rotation = Quaternion.Euler(0, right ? 0 : 180, 0);
+    }
+    void OnDrawGizmos() {
+        Gizmos.color = Color.red;
+
+        Gizmos.DrawLine(new Vector3(transform.position.x + groundOffset.x, transform.position.y - groundOffset.y + 0.1f, 0), new Vector2(transform.position.x + groundOffset.x, transform.position.y - groundOffset.y));
+        Gizmos.DrawLine(new Vector3(transform.position.x - groundOffset.x, transform.position.y - groundOffset.y + 0.1f, 0), new Vector2(transform.position.x - groundOffset.x, transform.position.y - groundOffset.y));
+        Gizmos.DrawWireSphere((Vector2)transform.position + rightOffset, collisionRadius);
+        Gizmos.DrawWireSphere((Vector2)transform.position + leftOffset, collisionRadius);
+
+    }
+    void CreateDust(){
+        dust.Play();
+    }
+}
