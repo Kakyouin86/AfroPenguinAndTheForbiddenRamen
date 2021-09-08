371e303b1b4844198e91511f133bebaef74e6022
Leonardo Jorge Monge <leo.monge1986@gmail.com>
2021-09-08 17:42:01 -0300

    2

--- AfroPenguin Goes 2D v1.0/Assets/Scripts/PlayerController.cs
+++ AfroPenguin Goes 2D v1.0/Assets/Scripts/PlayerController.cs
@@ -6,27 +6,35 @@ public class PlayerController : MonoBehaviour
 {
     public static PlayerController instance;
     [Header("Movement")]
-    public float moveSpeed = 8.0f;
-    //public Rigidbody2D theRB;
-    public float jumpForce = 15.0f;
-    public float doubleJumpForce = 15.0f;
+    private float horizontalValue;
+    //public float moveSpeed = 8.0f;
+    public float moveSpeed = 4.0f;
+    public float moveSpeedModifier = 1.5f;
+    public bool isRunning;
     public LayerMask whatIsGround;
-    public bool isGrounded;
-    public bool wasDoubleJump;
-    public bool canDoubleJump;
-    public bool stopInput;
-    public bool facingRight;
     public Transform groundCheckPoint;
-    //public Vector2 jumpHeight;
+    public float groundCheckRadius = 0.2f;
+    public bool isGrounded = true;
+    public float jumpForce = 15.0f;
+    public int availableJumps;
+    public int totalJumps = 2;
+    public bool multipleJumps;
+    public bool canMultipleJump;
+    public bool wasDoubleJump;
+    public bool coyoteJump;
+    public bool stopInput;
+    public bool facingRight = true;
 
     [Header("Animations & Sprite")]
     private Animator anim;
     public SpriteRenderer sr;
+    //public Rigidbody2D theRB;
 
     [Header("Knockback")]
     public float knockbackLenght = 0.25f;
     public float knockbackForce = 5.0f;
     public float knockbackCounter;
+    public bool isKnockback;
 
     [Header("Bounce After Jump")]
     public float bounceForce = 15.0f;
@@ -54,6 +62,7 @@ public class PlayerController : MonoBehaviour
     private void Awake()
     {
         instance = this;
+        availableJumps = totalJumps;
     }
     void Start()
     {
@@ -66,97 +75,88 @@ public class PlayerController : MonoBehaviour
         if (!PauseMenu.instance.isPaused && !stopInput)
         //if the pause is false (we are playing) AND, we haven't stopped our input, then... move the player. If one of them true, we can't do ANY of this stuff. If both are true, then it won't move.
         {
-            MovePlayer();
-            //Debug.Log(transform.position);
+            if (CanMoveOrInteract() == false)
+                return;
+            GroundCheck();
+            MovePlayer(horizontalValue);
+            Jump();
         }
     }
-    void MovePlayer()
+
+    void FixedUpdate()
     {
-        if (knockbackCounter <= 0)
+
+    }
+
+    public bool CanMoveOrInteract()
+    {
+        bool can = true;
+        if (isKnockback)
+            can = false;
+        return can;
+    }
+
+    public void GroundCheck()
+    {
+        bool wasGrounded = isGrounded;
+        isGrounded = false;
+        //Check if I'm grounded
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPoint.position, groundCheckRadius, whatIsGround);
+        if (colliders.Length > 0)
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
-            isGrounded = Physics2D.OverlapCircle(groundCheckPoint.position, 0.2f, whatIsGround);
-
-            if (isGrounded)
+            isGrounded = true;
+            anim.SetBool("isGrounded", true);
+            if (!wasGrounded)
             {
-                canDoubleJump = true;
-                wasDoubleJump = false;
-                //NEW
-                //hangCounter = hangTime;
-                //Stop NEW
+                availableJumps = totalJumps;
+                multipleJumps = false;
             }
-
-            //NEW
-            //else
-            //{
-            //hangCounter -= Time.deltaTime;
-            //}
-            //Stop NEW
-
-            if (Input.GetButtonDown("Jump")) //&& hangCounter > 0f)  //Second part is NEW and it's for the hangCounter in case double jump is not an option.
-            {
-                if (isGrounded)
-                {
-                    GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpForce);
-                    AudioManager.instance.PlaySFX(10);
-                    isGrounded = false;
-                    wasDoubleJump = false;
-                    CreateDust();
-                }
-
-                else if (canDoubleJump)
-                {
-                    wasDoubleJump = true;
-                    GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, doubleJumpForce);
-                    canDoubleJump = false;
-                    AudioManager.instance.PlaySFX(10);
-                }
-            }
-
-            //NEW:
-            if (Input.GetButtonUp("Jump") && GetComponent<Rigidbody2D>().velocity.y > 0)
-            {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y * 0.5f);
-            }
-            //Stop NEW
-
-            if (GetComponent<Rigidbody2D>().velocity.x != 0) //si la velocidad es 0, no hacer nada
-            {
-                bool prevFacing = facingRight; //Guardo el estado anterior para poder compararlo luego, para hacer el humito
-                bool isNegativeVelocity = GetComponent<Rigidbody2D>().velocity.x < 0; //la velocidad es negativa?
-                sr.flipX = isNegativeVelocity; // filpX es igual a lo anterior
-                facingRight = !isNegativeVelocity; //facingRight es lo contrario a flipX
-                if (facingRight != prevFacing) // comparo si el booleano cambió o no para ver si triggerear el humito
-                {
-                    CreateDust();
-                }
-            }
-
-            //if (GetComponent<Rigidbody2D>().velocity.x < 0)
-            //{
-            //    sr.flipX = true;
-            //    facingRight = false;
-            //}
-            //else if (GetComponent<Rigidbody2D>().velocity.x > 0)
-            //{
-            //    sr.flipX = false;
-            //    facingRight = true;
-            //}
         }
-
         else
         {
-            knockbackCounter -= Time.deltaTime;
-            //this counts down my time.
-            if (!sr.flipX)
-            //if true, we are facing to the left. If false, facing to the right.
-            {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(-knockbackForce, GetComponent<Rigidbody2D>().velocity.y);
+            //Un-parent the transform
+            anim.SetBool("isGrounded", false);
+            transform.parent = null;
+            if (wasGrounded)
+                StartCoroutine(CoyoteJumpDelay());
+        }
+    }
 
-            }
-            else
+    void MovePlayer(float dir)
+    {
+        //Set the yVelocity Value
+        anim.SetFloat("yVelocity", GetComponent<Rigidbody2D>().velocity.y);
+
+        horizontalValue = Input.GetAxisRaw("Horizontal");
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            isRunning = true;
+        }
+        if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            isRunning = false;
+        }
+        float xVal = dir * moveSpeed * 100 * Time.fixedDeltaTime;
+        //If we are running mulitply with the running modifier (higher)
+        if (isRunning)
+            xVal *= moveSpeedModifier;
+        //Create Vec2 for the velocity
+        Vector2 targetVelocity = new Vector2(xVal, GetComponent<Rigidbody2D>().velocity.y);
+        //Set the player's velocity
+        GetComponent<Rigidbody2D>().velocity = targetVelocity;
+        //Set the float xVelocity according to the x value of the RigidBody2D velocity 
+        anim.SetFloat("moveSpeed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
+
+        //Do the "Dust" effect when switching positions (or jumping)
+        if (GetComponent<Rigidbody2D>().velocity.x != 0) //si la velocidad es 0, no hacer nada
+        {
+            bool prevFacing = facingRight; //Guardo el estado anterior para poder compararlo luego, para hacer el humito
+            bool isNegativeVelocity = GetComponent<Rigidbody2D>().velocity.x < 0; //la velocidad es negativa?
+            sr.flipX = isNegativeVelocity; // filpX es igual a lo anterior
+            facingRight = !isNegativeVelocity; //facingRight es lo contrario a flipX
+            if (facingRight != prevFacing) // comparo si el booleano cambió o no para ver si triggerear el humito
             {
-                GetComponent<Rigidbody2D>().velocity = new Vector2(knockbackForce, GetComponent<Rigidbody2D>().velocity.y);
+                CreateDust();
             }
         }
 
@@ -165,16 +165,73 @@ public class PlayerController : MonoBehaviour
         {
             camTarget.localPosition = new Vector3(Mathf.Lerp(camTarget.localPosition.x, aheadAmount * Input.GetAxisRaw("Horizontal"), aheadSpeed * Time.deltaTime), camTarget.localPosition.y, camTarget.localPosition.z);
         }
+    }
 
-        anim.SetFloat("moveSpeed", Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
-        anim.SetBool("isGrounded", isGrounded);
+
+    public void Jump()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            if (isGrounded)
+            {
+                multipleJumps = true;
+                availableJumps--;
+                GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpForce;
+                CreateDust();
+            }
+            else
+            {
+                if (coyoteJump)
+                {
+                    multipleJumps = true;
+                    availableJumps--;
+                    GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpForce;
+                }
+
+                if (multipleJumps && availableJumps > 0)
+                {
+                    availableJumps--;
+                    GetComponent<Rigidbody2D>().velocity = Vector2.up * jumpForce;
+                }
+            }
+        }
+        if (Input.GetButtonUp("Jump") && GetComponent<Rigidbody2D>().velocity.y > 0)
+        {
+           // isGrounded = false;
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, GetComponent<Rigidbody2D>().velocity.y * 0.5f);
+        }
+    }
+
+    IEnumerator CoyoteJumpDelay()
+    {
+        coyoteJump = true;
+        yield return new WaitForSeconds(0.2f);
+        coyoteJump = false;
     }
 
     public void KnockBack()
     {
         knockbackCounter = knockbackLenght;
+        StartCoroutine(KnockBackDelay());
         GetComponent<Rigidbody2D>().velocity = new Vector2(0f, knockbackForce);
         anim.SetTrigger("hurt");
+        knockbackCounter -= Time.deltaTime;
+        //this counts down my time.
+        if (!sr.flipX)
+        //if true, we are facing to the left. If false, facing to the right.
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-knockbackForce, GetComponent<Rigidbody2D>().velocity.y);
+        }
+        else
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(knockbackForce, GetComponent<Rigidbody2D>().velocity.y);
+        }
+    }
+    IEnumerator KnockBackDelay()
+    {
+        isKnockback = true;
+        yield return new WaitForSeconds(knockbackCounter);
+        isKnockback = false;
     }
 
     public void Bounce()
