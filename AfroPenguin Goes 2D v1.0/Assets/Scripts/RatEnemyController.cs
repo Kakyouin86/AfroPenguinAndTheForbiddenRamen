409875d35ec02959d37b4815ff14ee21cca9a3ce
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2021-10-25 11:46:08 -0300

    1

--- AfroPenguin Goes 2D v1.0/Assets/Scripts/RatEnemyController.cs
+++ AfroPenguin Goes 2D v1.0/Assets/Scripts/RatEnemyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyController : MonoBehaviour
+public class RatEnemyController : MonoBehaviour
 {
     public float moveSpeed = 5.0f;
     public Transform leftPoint;
