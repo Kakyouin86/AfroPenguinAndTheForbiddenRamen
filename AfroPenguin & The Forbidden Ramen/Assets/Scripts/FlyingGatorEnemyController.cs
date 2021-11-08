624aea38aead4bb5ae38aab4563dab7105c3d3bc
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2021-11-08 09:48:41 -0300

    1

--- AfroPenguin & The Forbidden Ramen/Assets/Scripts/FlyingGatorEnemyController.cs
+++ AfroPenguin & The Forbidden Ramen/Assets/Scripts/FlyingGatorEnemyController.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FlyingGator : MonoBehaviour
+public class FlyingGatorEnemyController : MonoBehaviour
 {
 
     public Transform[] points;
