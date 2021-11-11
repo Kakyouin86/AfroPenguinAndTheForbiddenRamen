2cd9a8e8a81621c94dc98a0256784c886419a811
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2021-11-11 12:05:44 -0300

    1

--- AfroPenguin & The Forbidden Ramen v1.0/Assets/BossBearEnemyController.cs
+++ AfroPenguin & The Forbidden Ramen v1.0/Assets/BossBearEnemyController.cs
@@ -13,6 +13,7 @@ public class BossBearEnemyController : MonoBehaviour
     public float timeBetweenRun;
     //public Transform leftPoint;
    // public Transform rightPoint;
+    public bool isOnTheRight;
     public Transform leftPointWanderLeft;
     public Transform rightPointWanderLeft;
     public Vector2 newWanderLeft;
@@ -21,7 +22,6 @@ public class BossBearEnemyController : MonoBehaviour
 
     [Header("Ground Check")]
     public bool isGrounded;
-    public bool cameFromTheGround;
     public LayerMask whatIsGround;
     public float groundCheckRadius = 0.2f;
     public Vector2 bottomOffset;
@@ -34,29 +34,41 @@ public class BossBearEnemyController : MonoBehaviour
         //rightPoint.parent = null;
         leftPointWanderLeft.parent = null;
         rightPointWanderLeft.parent = null;
-
     }
 
     void Update()
     {
         isGrounded = Physics2D.OverlapCircle((Vector2)transform.position + bottomOffset, groundCheckRadius, whatIsGround);
+        if (isGrounded)
+        {
+            if (Input.GetKeyDown(KeyCode.I))
+            {
+                invisibleLength = Random.Range(3f, 6f);
+                invisibleCounter = invisibleLength;
+                WanderOnTheLeft();
+            }
 
-        if (Input.GetKeyDown(KeyCode.I))
-        {
-            WanderOnTheLeft();
-        }
-        if (invisibleCounter > 0)
-        {
-            invisibleCounter -= Time.deltaTime;
-            transform.position = Vector2.MoveTowards(transform.position, new Vector2(newWanderLeft.x, transform.position.y), walkSpeed * Time.deltaTime);
+            if (invisibleCounter > 0)
+            {
+                invisibleCounter -= Time.deltaTime;
+                transform.position = Vector2.MoveTowards(transform.position, new Vector2(newWanderLeft.x, transform.position.y), walkSpeed * Time.deltaTime);
+                theAnimator.SetBool("isWalking", true);
+                if (Vector2.Distance(transform.position, new Vector2(newWanderLeft.x, transform.position.y)) < .05f)
+                {
+                    WanderOnTheLeft();
+                }
+            }
+
+            else
+            {
+                theAnimator.SetBool("isWalking", false);
+            }
         }
     }
 
     void WanderOnTheLeft()
     {
-        newWanderLeft.x = Random.Range(leftPointWanderLeft.position.x, rightPointWanderLeft.position.x);
-        invisibleLength = Random.Range(1f, 2f);
-        invisibleCounter = invisibleLength;
+        newWanderLeft.x = Mathf.RoundToInt(Random.Range(leftPointWanderLeft.position.x,rightPointWanderLeft.position.x));
     }
 
     #region Ground Check Gizmo
