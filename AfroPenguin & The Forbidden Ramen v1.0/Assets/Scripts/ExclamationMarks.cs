cf56bc6bf8fa3037189f4bfda6bbc24dba788222
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2021-11-18 14:53:03 -0300

    1

---
+++ AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/ExclamationMarks.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExclamationMarks : MonoBehaviour
+{
+    public Animator theAnimator;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theAnimator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.CompareTag("Player") || player.CompareTag("Invulnerable"))
+        {
+            theAnimator.Play("Exclamation Mark - 01 - Opening Action");
+            theAnimator.SetBool("isOpened", true);
+        }
+    }
+
+    public void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.CompareTag("Player") || player.CompareTag("Invulnerable"))
+        {
+            theAnimator.Play("Exclamation Mark - 01 - Closing Action");
+            theAnimator.SetBool("isOpened", false);
+        }
+    }
+}
