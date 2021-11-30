124ae01ff61b28de63fa702bc26de2d1d2eb6981
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2021-11-30 20:08:40 -0300

    1

---
+++ AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/AnimatorFunctions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorFunctions : MonoBehaviour
+{
+    public MenuButtonController menuButtonController;
+	public bool disableOnce;
+
+	void PlaySound(AudioClip whichSound)
+    
+    {
+		if(!disableOnce)
+        
+        {
+			menuButtonController.audioSource.PlayOneShot (whichSound);
+		}
+        
+        else
+        
+        {
+			disableOnce = false;
+		}
+	}
+}	
---
+++ AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/MenuButton.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UIElements;
+using UnityEngine.UI;
+using Button = UnityEngine.UI.Button;
+
+public class MenuButton : MonoBehaviour
+{
+    public MenuButtonController menuButtonController;
+    public Animator theAnimator;
+    public AnimatorFunctions animatorFunctions;
+    public int thisIndex;
+    public Button thisButton;
+
+    void Start()
+    {
+        theAnimator = GetComponent<Animator>();
+        animatorFunctions = GetComponent<AnimatorFunctions>();
+        thisButton = GetComponent<Button>();
+    }
+    void Update()
+    {
+		if(menuButtonController.index == thisIndex)
+		{
+            theAnimator.SetBool ("selected", true);
+
+			if(Input.GetAxis ("Submit") == 1)
+			{
+                theAnimator.SetBool ("pressed", true);
+                StartCoroutine("LittleFade");
+            }
+
+			else if (theAnimator.GetBool ("pressed"))
+			{
+                theAnimator.SetBool ("pressed", false);
+				animatorFunctions.disableOnce = true;
+			}
+		}
+
+		else
+		{
+            theAnimator.SetBool ("selected", false);
+		}
+    }
+
+    IEnumerator LittleFade()
+
+    {
+        yield return new WaitForSeconds(0.7f);
+        thisButton.onClick.Invoke();
+    }
+}
