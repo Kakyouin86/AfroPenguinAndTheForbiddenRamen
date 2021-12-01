9b8368a61b79534bd4644de931b5f52c40c9b08e
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2021-12-01 18:33:01 -0300

    1

---
+++ AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/InfiniteBackgroundLoop.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InfiniteBackgroundLoop : MonoBehaviour
+{
+    public float speed;
+
+    public Vector2 startPosition;
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.down * speed * Time.deltaTime);
+        if (transform.position.y <= -130)
+        {
+            transform.position = startPosition;
+        }
+    }
+}
