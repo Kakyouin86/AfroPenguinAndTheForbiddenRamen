e21c9903f3e0f85d551fc97c9f3abb07e30601b9
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2022-01-07 19:28:27 -0300

    1

---
+++ AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/CircularMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CircularMovement : MonoBehaviour
+{
+    public Transform rotationCenter;
+    public float rotationRadius = 2f, angularSpeed = 2f;
+    public float posX, posY, angle;
+    public bool moveRight;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (!moveRight)
+        {
+            posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+            posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+            transform.position = new Vector2(posX, posY);
+            angle = angle + Time.deltaTime * -angularSpeed;
+
+            if (angle >= 360f)
+                angle = 0f;
+        }
+
+        else
+        {
+            posX = rotationCenter.position.x + Mathf.Cos(angle) * rotationRadius;
+            posY = rotationCenter.position.y + Mathf.Sin(angle) * rotationRadius;
+            transform.position = new Vector2(posX, posY);
+            angle = angle + Time.deltaTime * angularSpeed;
+
+            if (angle >= 360f)
+                angle = 0f;
+        }
+
+    }
+}
