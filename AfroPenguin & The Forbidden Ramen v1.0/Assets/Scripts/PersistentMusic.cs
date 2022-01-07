8a1128524e55ad9d9db4f44b92970ae9e16d8989
Kakyouin86 <72977925+Kakyouin86@users.noreply.github.com>
2022-01-07 17:59:21 -0300

    1

--- AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/PersistentMusic.cs
+++ AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/PersistentMusic.cs
@@ -1,18 +1,53 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PersistentMusic : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public AudioSource _audioSource;
+    public GameObject[] other;
+    public bool NotFirst = false;
+    public static PersistentMusic instance;
+
+    private void Awake()
     {
-        
+        instance = this;
+        other = GameObject.FindGameObjectsWithTag("Music");
+
+        foreach (GameObject oneOther in other)
+        {
+            if (oneOther.scene.buildIndex == -1)
+            {
+                NotFirst = true;
+            }
+        }
+
+        if (GameObject.FindGameObjectsWithTag("Music").Length > 1)
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(transform.gameObject);
+        _audioSource = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    public void PlayMusic()
     {
-        
+        if (_audioSource.isPlaying) return;
+        _audioSource.Play();
+    }
+
+    public void StopMusic()
+    {
+        _audioSource.Stop();
+    }
+
+    public void Update()
+    {
+        if (SceneManager.GetActiveScene().name == "Main Menu")
+        {
+            Destroy(gameObject);
+        }
     }
 }
