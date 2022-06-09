74fc6871a03fc1dbc730133ca9305be17e76176b
Rodrigo <rodrifs@gmail.com>
2022-06-09 20:07:41 +0200

    [Changed] Boss Battle Slime collider and player tracking

--- AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/BossSlimeJump.cs
+++ AfroPenguin & The Forbidden Ramen v1.0/Assets/Scripts/BossSlimeJump.cs
@@ -9,12 +9,12 @@ public class BossSlimeJump : StateMachineBehaviour
     public float jumpTimer;
     public float minTime = 1f;
     public float maxTime = 1.5f;
-    public Transform playerPosition;
+    Vector2 target;
     public float speed = 4f;
 
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
-        playerPosition = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+
         stateTimer = Random.Range(minTime, maxTime);
     }
 
@@ -28,8 +28,8 @@ public class BossSlimeJump : StateMachineBehaviour
         {
             stateTimer -= Time.deltaTime;
         }
-        
-            Vector2 target = new Vector2(playerPosition.position.x, animator.transform.position.y);
-            animator.transform.position = Vector2.MoveTowards(animator.transform.position, target, speed * Time.deltaTime);
+        Vector3 playerPosition = animator.gameObject.GetComponent<BossBattleSlime>().targetPosition;
+        target = new Vector2(playerPosition.x, animator.transform.position.y);
+        animator.transform.position = Vector2.MoveTowards(animator.transform.position, target, speed * Time.deltaTime);
     }
 }
